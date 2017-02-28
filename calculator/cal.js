0c0ee07e7f99a3a10be75b3cffc21e813989711c
Clear LI <lqb153105@gmail.com>
2017-03-01 01:10:02 +0800

    Revert "Revert "a simple and ugly calculator""
    This reverts commit b71b21f434c05d514ea6c48769018494365c6d15.

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,34 +1,49 @@
-var result = 0;
+var result = 0; //set a default value
+
+var judge = 0; //judge users whether type two or more sign(+-*/)
 
 function command(num){
 	if (result == 0){
 		result = num;
-	}
-	if !(result == 0){
-		result = num +
-	}
-	var rightNumber = num;
-	result = num;
-	document.getElementById("result").value = result;
+	} //change default result to num
+	else{
+		result = result + [num];
+	} //change result to string
+	document.getElementById("result").value = result;  //out put/display the result
+	judge = 1; //when judge=1 users can tpye sign(+-*/)
 }
 
-function plus(){
-	calculate();
-	operate = 1;
-	calcul = 1;
+function sign(num){
+	if (result == 0){
+		alert("error!");
+	} //users can not type sign(+-*/) as the first thing
+	else if(judge == 0){
+		alert("error!");
+	} //users can not continuously type two or more sign(+-*/)
+	else{
+		if(num == 1){
+		result = result + ["+"];
+	    }
+	    if(num == 2){
+		result = result + ["-"];
+	    }
+	    if(num == 3){
+		result = result + ["*"];
+	    }
+	    if(num == 4){
+		result = result + ["/"];
+	    }
+	}
+	document.getElementById("result").value = result; //out put/display the result
+	judge = 0; //when judge=0 users can not tpye sign(+-*/)
 }
 
 function equal(){
-	calculate();
-	operate= 1;
-	num = 0;
-	result = 0;
-	numshow = "0";
+	result = eval(result); //run function-evaluation 
+	document.getElementById("result").value = result; //out put/display the result
 }
 
-function calculate(){
-	numshow = Number(document.calculator.numScreen.value);
-	switch(calcul){
-		case 1:result=num+numshow;
-	}
+function clean(){
+	result = 0; //clean the result
+	document.getElementById("result").value = result; //out put/display the result
 }
